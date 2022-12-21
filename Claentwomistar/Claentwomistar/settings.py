14f970c13518d17ec6b8c0edb372c12d264fd7fb
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-21 14:16:24 -0300

    a

--- Claentwomistar/Claentwomistar/settings.py
+++ Claentwomistar/Claentwomistar/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Principal'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Claentwomistar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'Claentwomistar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'coloreze_ColorEz', # database name
+        'USER': 'coloreze_coloreze', # user with access to that database
+        'PASSWORD': 'Kkdvk122*', # password for the user
+        'HOST': 'colorez.es',   # if your database is hosted in another server then you should specify the host's IP
+        'PORT': '3306', # default MySQL port
     }
 }
 
@@ -123,3 +129,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'Principal.Usuario'
+LOGIN_URL = 'Login'
+LOGOUT_URL = 'logout'
+LOGIN_REDIRECT_URL = '/Inicio/'
+LOGOUT_REDIRECT_URL = 'Login'
--- Claentwomistar/Claentwomistar/urls.py
+++ Claentwomistar/Claentwomistar/urls.py
@@ -1,4 +1,4 @@
-"""Claentwomistar URL Configuration
+"""JustCats URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Principal.views import index,Inicio,RegisterView,LoginView,LogoutView,perfil
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='Index'),
+    path('Inicio/', Inicio, name='Inicio'),
+    path('Perfil/', perfil,name='perfil'),
+    path('Register/',RegisterView.as_view(), name='Register'),
+    path('Login/', LoginView.as_view(), name='Login'),
+    path('logout/', LogoutView,name='Logout'),
 ]
