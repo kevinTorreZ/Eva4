42ec991dcd7e456f7247fce6ec78eddc1ca314d8
kevinTorreZ <61859021+kevinTorreZ@users.noreply.github.com>
2022-12-22 04:36:58 -0300

    a

--- Claentwomistar/Administracion/models.py
+++ Claentwomistar/Administracion/models.py
@@ -1,14 +1,16 @@
 from django.db import models
-
+import os
 class Productos(models.Model):
     id = models.AutoField(primary_key=True)
     Nombre = models.CharField(max_length=50)
     Valor = models.IntegerField()
     Stock = models.IntegerField()
-    photo = models.ImageField(upload_to = 'Principal/static',unique=False, height_field = None, width_field = None, max_length = 100, help_text = None)
+    photo = models.ImageField(upload_to = 'Principal/static/imgProductos',unique=False, height_field = None, width_field = None, max_length = 100, help_text = None)
     Ventas = models.IntegerField(default=0)
     Marca = models.CharField(max_length=25)
     Descripcion = models.TextField(max_length=250)
     Modelo = models.CharField(max_length=50)
     def __str__(self):
         return str(self.id)+" "+self.Nombre
+    def filename(self):
+        return os.path.basename(self.photo.name)
